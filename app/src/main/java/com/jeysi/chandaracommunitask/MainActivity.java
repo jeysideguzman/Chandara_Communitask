1049e6fd229c2075a6cf8f032c6dc6fcbead2a79
John Carlo De Guzman <johncarlobdeguzman@iskolarngbayan.pup.edu.ph>
2023-07-14 20:01:10 +0800

    postingg

--- app/src/main/java/com/jeysi/chandaracommunitask/MainActivity.java
+++ app/src/main/java/com/jeysi/chandaracommunitask/MainActivity.java
@@ -40,10 +40,93 @@ public class MainActivity extends AppCompatActivity {
 
 
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.homenav));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.addposticon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.searchicon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.notificon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.notiffill));
 
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.homenav));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.homenav));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.homenav));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.homenav);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                switch (tab.getPosition()) {
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.homenav);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.searchicon);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.addposticon);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.notiffill);
+                        break;
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(android.R.drawable.ic_menu_help);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+                switch (tab.getPosition()) {
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.homenav);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.searchicon);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.addposticon);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.notificon);
+                        break;
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.notiffill);
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+                switch (tab.getPosition()) {
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.homenav);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.searchicon);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.addposticon);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.notificon);
+                        break;
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.notiffill);
+                        break;
+
+                }
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/jeysi/chandaracommunitask/fragments/Home.java
+++ app/src/main/java/com/jeysi/chandaracommunitask/fragments/Home.java
@@ -2,28 +2,81 @@ package com.jeysi.chandaracommunitask.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jeysi.chandaracommunitask.R;
+import com.jeysi.chandaracommunitask.adapter.HomeAdapter;
+import com.jeysi.chandaracommunitask.model.HomeModel;
+
+import java.lang.annotation.Documented;
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlinx.coroutines.internal.LocalAtomics_commonKt;
 
 
 public class Home extends Fragment {
 
+    private RecyclerView recyclerView;
+    HomeAdapter adapter;
+
+    private List<HomeModel> list;
+
+    private FirebaseUser user;
+    DocumentReference reference;
+
+
     public Home() {
         // Required empty public constructor
     }
-
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+        reference = FirebaseFirestore.getInstance().collection("Posts").document(user.getUid());
+        list = new ArrayList<>();
+        adapter = new HomeAdapter(list, getContext());
+        recyclerView.setAdapter(adapter);
+
+        loadDataFromFirestore();
+
+    }
+
+    private void init(View view){
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+    }
+
+    private void loadDataFromFirestore(){
+
+
+
     }
 }
